331f3db08e4269e2581d0d9ff139daf0cefefcca
Lucklyric <xinyao1@ualberta.ca>
2016-02-17 14:48:52 -0700

    1. Add new debugflag for printing

--- apriori_serial.cpp
+++ apriori_serial.cpp
@@ -35,6 +35,9 @@ vector<vector<int>> DB; 										//database
 typedef unordered_map<vector<int>,int,int_vector<vector<int>>> u_map_vector;
 unordered_map <int,u_map_vector> candidates_k;
 unordered_map <int,u_map_vector> large_itemsets_k;
+clock_t lastTimeTick,mainstart;
+int debug_flag = 0;
+
 
 /**
  * Declare all functions
@@ -46,48 +49,49 @@ void foreachDB(u_map_vector &candidates);
 void nestCheckSubset(vector<int> &transaction,int level,vector<int>&pre,int idx);
 void output(u_map_vector &map);
 void output(vector<int> &trans);
+void timetick(clock_t since);
 /**
  * Main application
  */
 int main(int argc, char const *argv[]){
-	clock_t begin = clock();
+	mainstart = lastTimeTick = clock();
 	/*Start loading data and initialize C1 and L1*/
-	if (inputdata(argv[1])) {printf("Input file success!\n");}
+	printf("Start finding frequent itemsets\n");
+	if (inputdata(argv[1])) {if (debug_flag) printf("Input file success!\n");}
 	while(true){
 		printf("Number of frequent %d_itemsets: %d\n",currentLevel,(int)large_itemsets_k[currentLevel].size());
 		currentLevel++;
 		u_map_vector candidates = generateCandidates(large_itemsets_k[currentLevel-1]);
 		if (candidates.size() == 0) {
-			printf("No any more candidates item-sets! at level %d\n",currentLevel);
+			if (debug_flag) printf("No any more candidates item-sets! at level %d\n",currentLevel);
 			break;
 		}
 		candidates_k[currentLevel] = candidates;
-		printf("Number of candidates:%d\n",(int)candidates.size());
+		if (debug_flag)printf("Number of candidates:%d\n",(int)candidates.size());
 		foreachDB(candidates_k[currentLevel]);
 		//output(candidates_k[currentLevel]);
 		u_map_vector largeItemsets = generateLargeItemsets(candidates_k[currentLevel]);
-		printf("Number of largeItemsets:%d\n",(int)largeItemsets.size());
+		if (debug_flag)printf("Number of largeItemsets:%d\n",(int)largeItemsets.size());
 		if (largeItemsets.size() == 0) {
-			printf("No any more large item-sets! at level %d\n",currentLevel);
+			if (debug_flag)printf("No any more large item-sets! at level %d\n",currentLevel);
 			break;
 		}
 		large_itemsets_k[currentLevel] = largeItemsets;
 	}
-	clock_t end = clock();
-	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-	printf("Total spend %f s\n",elapsed_secs);
+
+	printf("End finding frequent itemsets ");
+	timetick(mainstart);
 
 	return 0;
 }
 
 bool inputdata(const char* filename){
-	printf("Start loading data...\n");
+	if (debug_flag) printf("Input data...");
 	u_map_vector candidates;
-	clock_t begin = clock();
 	ifstream inputFile;
 	inputFile.open(filename);
 	if(!inputFile){
-		printf("Input file error");
+		printf("Input file error\n");
 		return false;
 	}
 	string line ;
@@ -112,17 +116,18 @@ bool inputdata(const char* filename){
 		tmpLine.clear();
 	}
 	inputFile.close();
-	clock_t end = clock();
 	num_transactions = DB.size();
-	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 	candidates_k[1] = candidates;
 	large_itemsets_k[1] = generateLargeItemsets(candidates);
-	printf("Spend %f s\n",elapsed_secs);
+	if (debug_flag) {
+		printf("Finshed");
+		timetick(lastTimeTick);
+	}
 	return true;
 }
 
 u_map_vector generateLargeItemsets(u_map_vector &candidates){
-	printf("Start generating (%d)largeItemsets...\n",currentLevel);
+	if (debug_flag)printf("Start generating (%d)largeItemsets...\n",currentLevel);
 	int tresh = min_support*num_transactions;
 	u_map_vector largeItemsets;
 
@@ -131,13 +136,13 @@ u_map_vector generateLargeItemsets(u_map_vector &candidates){
 			largeItemsets[itemset->first] = itemset->second;
 		}
 	}
-	printf("End generating (%d)largeItemsets!\n",currentLevel);
+	if (debug_flag)printf("End generating (%d)largeItemsets!\n",currentLevel);
 
 	return largeItemsets;
 }
 
 u_map_vector generateCandidates(u_map_vector &largeItemsets){
-	printf("Start generating (k=%d) candidates...\n",currentLevel);
+	if (debug_flag)printf("Start generating (k=%d) candidates...\n",currentLevel);
 	u_map_vector candidates;
 	u_map_vector::iterator largeItemsets_end;
 	for(u_map_vector::iterator itemsetA = largeItemsets.begin();itemsetA!=largeItemsets_end;++itemsetA){
@@ -165,6 +170,7 @@ u_map_vector generateCandidates(u_map_vector &largeItemsets){
 					}else{
 						candidates[candidate] = 0;
 					}
+					//Old Prune Part
 					//sort(candidate.begin(),candidate.end());
 //					candidates[candidate] = 0;
 //					Prune
@@ -180,11 +186,12 @@ u_map_vector generateCandidates(u_map_vector &largeItemsets){
 //							candidates[candidate] = 0;
 //						}
 //					}
+					//Old Prune Part End
 				}
 			}
 		}
 	}
-	printf("End generating (k=%d) - (%d)candidates!\n",currentLevel,(int)candidates.size());
+	if (debug_flag)printf("End generating (k=%d) - (%d)candidates!\n",currentLevel,(int)candidates.size());
 	return candidates;
 }
 
@@ -211,7 +218,7 @@ void foreachDB(u_map_vector &candidates){
 //			}
 //		}
 //	}
-	printf("Start scaning database...\n");
+	if (debug_flag)printf("Start scaning database...");
 		int num = 0;
 		for(int i = 0 ; i < num_transactions;i++){
 			u_map_vector::iterator candidates_end = candidates.end();
@@ -223,7 +230,7 @@ void foreachDB(u_map_vector &candidates){
 
 			}
 		}
-	printf("End caning database!\n");
+		if (debug_flag)printf("End caning database!\n");
 }
 
 
@@ -248,6 +255,13 @@ void nestCheckSubset(vector<int> &transaction,int level,vector<int>&pre,int idx)
 /**
  * Util functions for testing
  */
+void timetick(clock_t since){
+	clock_t current = clock();
+	double elapsed_secs = double(current - since) / CLOCKS_PER_SEC;
+	lastTimeTick = current;
+	printf("spent %f s\n",elapsed_secs);
+}
+
 void output(u_map_vector &map){
 	for(u_map_vector::iterator itemset = map.begin();itemset!=map.end();++itemset){
 		printf("Candidates:");
